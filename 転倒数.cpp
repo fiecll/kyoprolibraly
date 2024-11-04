a43b3abf2a5bd42466618407349cf3fc7074cbb7
fie <doublerokko@yahoo.co.jp>
2024-11-04 15:18:46 +0900

    vscode を除いた

---
+++ 転倒数.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using ll = long long;
+using Graph = vector<vector<int>>;
+using P = pair<int,int>;
+template<typename T,typename U>inline bool chmax(T&a,U b)
+{if(a<b){a=b;return 1;}return 0;}
+template<typename T,typename U>inline bool chmin(T&a,U b)
+{if(a>b){a=b;return 1;}return 0;}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << n << endl;
+}
